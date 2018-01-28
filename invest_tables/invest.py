65eae1ea6ffa64a97c82675f1d08cabc3c0d456c
shreyasmahimkar <shreyasmahimkar@gmail.com>
2018-01-27 20:39:56 -0500

    adding acorns,fundrise,etc.

--- invest_tables/invest.py
+++ invest_tables/invest.py
@@ -6,12 +6,17 @@ import urllib3
 from datetime import datetime
 from forex_python.converter import CurrencyRates
 
+i = datetime.now()
+now = i.strftime('%Y-%m-%d ')
+
+def get_monthly_investment_factor(date_of_investment):
+    monthly_investment_factor = pd.to_datetime(now) - pd.to_datetime(date_of_investment)
+    mif = monthly_investment_factor.days // 30.0
+    return mif
 
 @app.route("/tables")
 def show_tables():
     # ------- Get Todays Date
-    i = datetime.now()
-    now = i.strftime('%Y-%m-%d ')
     c = CurrencyRates()
 
     data = pd.read_excel('data.xlsx')
@@ -38,20 +43,41 @@ def show_tables():
     deposit_temp.Money_Invested = deposit_temp.Money_Invested / c.get_rate('USD', 'INR')
     deposit_temp.Todays_Value = deposit_temp.Todays_Value / c.get_rate('USD', 'INR')
 
+    acorns_data = data.loc[data.Type == 'Acorns']
+    mif = get_monthly_investment_factor(acorns_data.Date_of_Investment.values[0])
+    acorns_data.Money_Invested = acorns_data.Money_Invested + (mif * 100.0)
+    acorns_data.Todays_Value = acorns_data.Money_Invested + (acorns_data.Money_Invested * 3) / 100.0
+    acorns_data.Maturity_Date = now
 
+    fundrise_data = data.loc[data.Type == 'Fundrise']
+    mif = get_monthly_investment_factor(fundrise_data.Date_of_Investment.values[0])
+    fundrise_data.Money_Invested = fundrise_data.Money_Invested + (mif * 100.0)
+    fundrise_data.Todays_Value = fundrise_data.Money_Invested + (fundrise_data.Money_Invested * 0.7) / 100.0
 
-    new_data = pd.concat([cryptocurrency_final,deposit_temp])
+    lendingClub_data = data.loc[data.Type == 'LendingClub']
+    mif = get_monthly_investment_factor(lendingClub_data.Date_of_Investment.values[0])
+    lendingClub_data.Money_Invested = lendingClub_data.Money_Invested + (mif * 100.0)
+    lendingClub_data.Todays_Value = lendingClub_data.Money_Invested + ((lendingClub_data.Money_Invested * 5.61) / 1200.0) * 0
+
+    new_data = pd.concat([cryptocurrency_final,deposit_temp,acorns_data,fundrise_data,lendingClub_data])
 
     new_data.set_index(['Name'], inplace=True)
     new_data.index.name=None
     cryptocurrency = new_data.loc[new_data.Type == 'Cryptocurrency']
     deposit = new_data.loc[new_data.Type == 'Deposit']
+    acorns = new_data.loc[new_data.Type == 'Acorns']
+    fundrise = new_data.loc[new_data.Type == 'Fundrise']
+    lendingClub = new_data.loc[new_data.Type == 'LendingClub']
 
     total = new_data.groupby('Currency').agg({'Money_Invested': 'sum', 'Todays_Value': 'sum'}).reset_index()
     return render_template('view.html', tables=[total.to_html(classes='Total'),
                                                 cryptocurrency.to_html(classes='Cryptocurrency'),
-                                                deposit.to_html(classes='Deposit')],
-                           titles=['na', 'Total' ,'Cryptocurrencies', 'Deposits'])
+                                                deposit.to_html(classes='Deposit'),
+                                                acorns.to_html(classes='Acorns'),
+                                                fundrise.to_html(classes='Fundrise'),
+                                                lendingClub.to_html(classes='LendingClub')],
+                           titles=['na', 'Total' ,'Cryptocurrencies', 'Deposits','Acorns','Fundrise','LendingClub'])
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    #app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True)
