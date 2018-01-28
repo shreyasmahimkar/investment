c0c4de754e3f303c8154ac166f0430f02476ce4d
shreyasmahimkar <shreyasmahimkar@gmail.com>
2018-01-28 17:10:09 -0500

    separating cryptocurrency

--- invest_tables/invest.py
+++ invest_tables/invest.py
@@ -8,26 +8,23 @@ from forex_python.converter import CurrencyRates
 
 i = datetime.now()
 now = i.strftime('%Y-%m-%d ')
+data = pd.read_excel('data.xlsx')
 
 def get_monthly_investment_factor(date_of_investment):
     monthly_investment_factor = pd.to_datetime(now) - pd.to_datetime(date_of_investment)
     mif = monthly_investment_factor.days // 30.0
     return mif
 
-@app.route("/tables")
-def show_tables():
-    # ------- Get Todays Date
-    c = CurrencyRates()
-
-    data = pd.read_excel('data.xlsx')
-    cryptocurrency_temp = data.loc[data.Type == 'Cryptocurrency']
+def get_cryptocurrency(df):
+    cryptocurrency_temp = df.loc[df.Type == 'Cryptocurrency']
     ## Get todays Value of cryptocurrencies
     http = urllib3.PoolManager()
     r = http.request('GET', 'https://api.coinmarketcap.com/v1/ticker/')
     all_data = json.loads(r.data.decode('utf-8'))
     all_data_df = pd.DataFrame(all_data)
 
-    cryptocurrency_popular = cryptocurrency_temp.merge(all_data_df[['id','price_usd']],left_on='Name',right_on='id',how='inner')
+    cryptocurrency_popular = cryptocurrency_temp.merge(all_data_df[['id', 'price_usd']], left_on='Name', right_on='id',
+                                                       how='inner')
     cryptocurrency_popular.Units = cryptocurrency_popular.Units.apply(float)
     cryptocurrency_popular.price_usd = cryptocurrency_popular.price_usd.apply(float)
     cryptocurrency_popular.Todays_Value = cryptocurrency_popular.price_usd * cryptocurrency_popular.Units
@@ -36,24 +33,54 @@ def show_tables():
 
     cryptocurrency_ico = cryptocurrency_temp[~cryptocurrency_temp.Name.isin(cryptocurrency_popular.Name.unique())]
     cryptocurrency_ico.Maturity_Date = 'TBD'
-    cryptocurrency_final = pd.concat([cryptocurrency_popular,cryptocurrency_ico])
+    cryptocurrency_final = pd.concat([cryptocurrency_popular, cryptocurrency_ico])
+    return cryptocurrency_final
 
+
+@app.route("/cryptocurrency")
+def show_cryptocurrency():
+    df = data.copy()
+    # Cryptocurrency
+    cryptocurrency_final = get_cryptocurrency(df)
+    new_data = pd.concat([cryptocurrency_final])
+    new_data.set_index(['Name'], inplace=True)
+    new_data.index.name = None
+    cryptocurrency = new_data.loc[new_data.Type == 'Cryptocurrency']
+    total = new_data.groupby('Currency').agg({'Money_Invested': 'sum', 'Todays_Value': 'sum'}).reset_index()
+    total = total[['Currency', 'Money_Invested', 'Todays_Value']]
+    return  render_template('view.html', tables=[total.to_html(classes='Total'),
+                                                cryptocurrency.to_html(classes='Cryptocurrency')],
+                           titles=['na', 'Total' ,'Cryptocurrencies'])
+
+@app.route("/tables")
+def show_tables():
+    # ------- Get Todays Date
+    c = CurrencyRates()
+    df = data.copy()
+
+    #Cryptocurrency
+    cryptocurrency_final = get_cryptocurrency(df)
+
+    #Deposit - RD FD ICICI
     deposit_temp = data.loc[data.Type == 'Deposit']
     deposit_temp.Currency = 'USD'
     deposit_temp.Money_Invested = deposit_temp.Money_Invested / c.get_rate('USD', 'INR')
     deposit_temp.Todays_Value = deposit_temp.Todays_Value / c.get_rate('USD', 'INR')
 
+    #Acorns Investment
     acorns_data = data.loc[data.Type == 'Acorns']
     mif = get_monthly_investment_factor(acorns_data.Date_of_Investment.values[0])
     acorns_data.Money_Invested = acorns_data.Money_Invested + (mif * 100.0)
     acorns_data.Todays_Value = acorns_data.Money_Invested + (acorns_data.Money_Invested * 3) / 100.0
     acorns_data.Maturity_Date = now
 
+    #Fundrise Investment
     fundrise_data = data.loc[data.Type == 'Fundrise']
     mif = get_monthly_investment_factor(fundrise_data.Date_of_Investment.values[0])
     fundrise_data.Money_Invested = fundrise_data.Money_Invested + (mif * 100.0)
     fundrise_data.Todays_Value = fundrise_data.Money_Invested + (fundrise_data.Money_Invested * 0.7) / 100.0
 
+    #LendingClub Investment
     lendingClub_data = data.loc[data.Type == 'LendingClub']
     mif = get_monthly_investment_factor(lendingClub_data.Date_of_Investment.values[0])
     lendingClub_data.Money_Invested = lendingClub_data.Money_Invested + (mif * 100.0)
@@ -70,6 +97,7 @@ def show_tables():
     lendingClub = new_data.loc[new_data.Type == 'LendingClub']
 
     total = new_data.groupby('Currency').agg({'Money_Invested': 'sum', 'Todays_Value': 'sum'}).reset_index()
+    total = total[['Currency', 'Money_Invested', 'Todays_Value']]
     return render_template('view.html', tables=[total.to_html(classes='Total'),
                                                 cryptocurrency.to_html(classes='Cryptocurrency'),
                                                 deposit.to_html(classes='Deposit'),
